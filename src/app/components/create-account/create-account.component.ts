afe2b2dae2181ae929dbc9715e244af86772bb02
eliyahu porush <eliyahuporush@gmail.com>
2020-09-02 09:51:18 +0300

    ...

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -12,12 +12,23 @@ export class CreateAccountComponent implements OnInit {
 
   ngOnInit(): void {
     this.createAccountForm = new FormGroup({
-      name: new FormControl(),
-      email: new FormControl(null, Validators.email),
-      phone: new FormControl(),
-      alies: new FormControl()
+      name: new FormControl(null, [Validators.required,Validators.minLength(2), Validators.maxLength(20)]),
+      email: new FormControl(null, [Validators.email,Validators.required]),
+      password1: new FormControl(null, [Validators.minLength(8),Validators.maxLength(15), Validators.required]),
+      password2: new FormControl(null, [Validators.minLength(8),Validators.maxLength(15), Validators.required]),
+      phone: new FormControl(null, Validators.minLength(10)),
+      alies: new FormControl(null)
     })
   }
-  onSubmit(){}
+  onSubmit(){
+    let form = this.createAccountForm.controls ;
+    if( (form.password1.value == form.password2.value) && form.valid){
+      console.log('form valid!');
+      
+    }
+    else console.log('form not valid')    
+    console.log(form.password2.value);
+    
+  }
 
 }
